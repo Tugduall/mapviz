0bc9f08baba53fa1b2e0dbd4fb2e3ead91795eab
Tugdual <tugdual.launay@dice-engineering.com>
2021-09-06 11:34:09 +0200

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,23 +3,13 @@
 # abort on errors
 set -e
 
-# build
 npm run build
 
-# navigate into the build output directory
 cd dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-git init
-git add -A
+git add dist
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Tugduall/mapviz.git main:gh-pages
+git subtree push --prefix dist origin gh-pages
 
 cd -
