87100a497d1858b006fabadaba5bcfe9a567ff57
Tugdual <launay.tug@gmail.com>
2021-09-06 10:51:34 +0200

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+import '/node_modules/primeflex/primeflex.css'
+import './assets/scss/global.scss'
+import PrimeVue from "primevue/config";
+import Tooltip from "primevue/tooltip";
+import router from './router'
+import ToastService from "primevue/toastservice";
+
+const app = createApp(App).use(router);
+
+app.use(PrimeVue);
+app.use(router)
+app.use(ToastService);
+app.directive('tooltip', Tooltip);
+app.mount('#app');
+
---
+++ src/views/Home.vue
@@ -0,0 +1,356 @@
+<template>
+  <Toast position="top-right"/>
+
+  <Menubar id="menu-bar">
+    <template #start>
+      <img id="logo" alt="Mapool logo" src="../assets/mapool_logo_3.png">
+    </template>
+    <template #end>
+      <!-- PayPal Donate -->
+      <form action="https://www.paypal.com/cgi-bin/webscr" class="flex align-items-center justify-content-center"
+            method="post">
+
+        <!-- Identify your business so that you can collect the payments. -->
+        <input name="business" type="hidden" value="launay.tug@gmail.com">
+
+        <!-- Specify a Donate button. -->
+        <input name="cmd" type="hidden" value="_donations">
+
+        <!-- Specify details about the contribution -->
+        <input name="item_name" type="hidden" value="Mapool development">
+        <input name="item_number" type="hidden" value="Help us to develop fancy new feature for Mapool">
+        <input name="currency_code" type="hidden" value="EUR">
+
+        <!-- Display the payment button. -->
+        <input alt="Donate" name="submit"
+               src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif"
+               type="image">
+        <img alt="" height="1" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif"
+             width="1">
+
+      </form>
+    </template>
+  </Menubar>
+
+  <div id="container">
+    <div id="mapContainer"></div>
+  </div>
+
+
+  <div id="side-container">
+
+    <Card class="t-card mb-2">
+      <template #title>
+        Waypoints
+      </template>
+      <template #content>
+        <div class="grid">
+          <div class="col-6">
+            <p class="m-0 mb-1 font-medium">Name</p>
+            <InputText v-model="currentWaypoints.name" required type="text"/>
+          </div>
+          <div class="col-6">
+            <p class="m-0 mb-1 font-medium">Color</p>
+            <div class="flex flex-row flex-nowrap align-items-center">
+              <ColorPicker v-model="currentWaypoints.color" class="mr-1"/>
+              <InputText v-model="currentWaypoints.color" type="text"/>
+            </div>
+          </div>
+          <div class="col-12">
+            <Textarea v-model="waypointInputValue" rows="6" @change="formatWaypoints"/>
+          </div>
+        </div>
+        <Button :disabled="waypointInputValue === ''" class="p-button-sm p-button-outlined p-button-help mr-1"
+                icon="pi pi-map-marker"
+                iconPos="left"
+                label="View waypoints"
+                @click="formatWaypoints"/>
+        <Button :disabled="waypointInputValue === ''" class="p-button-sm p-button-outlined" icon="pi pi-check"
+                iconPos="left" label="Save Waypoints"
+                @click="saveWaypoints"/>
+      </template>
+    </Card>
+
+    <Card class="t-card mb-2">
+      <template #title>
+        Waypoint lists
+      </template>
+      <template #content>
+
+        <div class="saved-waypoints-list">
+          <div v-for="(waypoint, index) in savedWaypoints" :key="'swp_'+index"
+               class="flex flex-row flex-nowrap align-items-center justify-content-between mb-2">
+            <div class="col-4 p-0">
+              <p class="capitalize m-0">{{ waypoint.name }}</p>
+            </div>
+            <div class="col-4 p-0">
+              <!--TODO-->
+              <p class="p-text-wrap">{{ waypoint.formattedLatLngs }}</p>
+            </div>
+            <div class="col-4 p-0 flex flex-row justify-content-around align-items-center">
+              <Dropdown v-if="waypoint.markers.length > 1" v-model="waypoint.shape" :options="shapes" optionLabel="name"
+                        @change="createFeatureGroup(waypoint)">
+                <template #value="shape">
+                  <div class="flex flex-row justify-content-center" v-html="shape.value.icon"></div>
+                </template>
+                <template #option="shape">
+                  <div class="flex flex-row justify-content-center" v-html="shape.option.icon"></div>
+                </template>
+              </Dropdown>
+              <Button class="p-button-sm p-button-danger p-button-outlined" icon="pi pi-trash" iconPos="left"
+                      @click="deleteSavedWaypoint(index)"/>
+            </div>
+          </div>
+        </div>
+
+        <p v-if="savedWaypoints.length === 0" class="text-center m-0 text-gray-500">No waypoints saved yet</p>
+      </template>
+    </Card>
+
+    <Accordion :multiple="true">
+      <AccordionTab header="Decimal Degree to Degree Minute Second">
+        <div class="flex flex-row align-items-center justify-content-around  pt-3">
+          <span class="p-float-label">
+              <InputNumber id="inputDecimalDegree" v-model="converter.ddInputValue" :minFractionDigits="2"
+                           mode="decimal"/>
+              <label for="inputDecimalDegree">Decimal Degree</label>
+          </span>
+          <Button label="Convert" @click="ddToDms(converter.ddInputValue)"/>
+        </div>
+        <p class="text-center">{{ converter.ddToDmsResult }}</p>
+      </AccordionTab>
+      <AccordionTab header="Degree Minutes Second to Decimal Degree">
+        <div class="flex flex-row justify-content-around pt-3">
+          <span class="p-float-label w-5rem">
+              <InputNumber id="inputDegree" v-model="converter.dmsInputValues.minutes"/>
+              <label for="inputDegree">Degree</label>
+          </span>
+          <span class="p-float-label w-5rem">
+              <InputNumber id="inputMinute" v-model="converter.dmsInputValues.minutes"/>
+              <label for="inputMinute">Minute</label>
+          </span>
+          <span class="p-float-label w-5rem">
+              <InputNumber id="inputSecond" v-model="converter.dmsInputValues.degrees"/>
+              <label for="inputSecond">Second</label>
+          </span>
+          <Button label="Convert" @click="dmsToDd(converter.dmsInputValues)"/>
+        </div>
+        <p class="text-center">{{ converter.dmsToDdResult }}</p>
+      </AccordionTab>
+    </Accordion>
+
+  </div>
+</template>
+
+<script>
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import Menubar from "primevue/menubar";
+import InputText from "primevue/inputtext";
+import ColorPicker from "primevue/colorpicker";
+import Button from "primevue/button";
+import Card from "primevue/card";
+import Textarea from "primevue/textarea";
+import Dropdown from "primevue/dropdown";
+import InputNumber from "primevue/inputnumber";
+import Accordion from "primevue/accordion";
+import AccordionTab from "primevue/accordiontab";
+import Toast from "primevue/toast";
+
+export default {
+  name: "Home",
+  components: {
+    Menubar, InputText, ColorPicker, Button, Card, Textarea, Dropdown, InputNumber, Accordion, AccordionTab, Toast,
+  },
+  data() {
+    return {
+      converter: {
+        ddInputValue: null,
+        ddToDmsResult: null,
+        dmsInputValues: {
+          degrees: null,
+          minutes: null,
+          seconds: null,
+        },
+        dmsToDdResult: null,
+      },
+      // Map
+      map: null,
+      center: [37, 7749, -122, 4194],
+      // Data
+      shapes: [
+        {
+          name: 'polylines',
+          icon: '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash" viewBox="0 0 16 16"><path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/></svg>'
+        },
+        {
+          name: 'rectangle',
+          icon: '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-square" viewBox="0 0 16 16"><path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/></svg>'
+        },
+        {
+          name: 'polygon',
+          icon: '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pentagon" viewBox="0 0 16 16"><path d="m8 1.288 6.842 5.56L12.267 15H3.733L1.158 6.847 8 1.288zM16 6.5 8 0 0 6.5 3 16h10l3-9.5z"/></svg>'
+        },
+        {
+          name: 'waypoints',
+          icon: '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16"><path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/><path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/></svg>'
+        },
+      ],
+      savedWaypoints: [],
+      waypointInputValue: "",
+      currentWaypoints: {
+        latLngs: [],
+        formattedLatLngs: "",
+        color: '1976D2',
+        name: '',
+        visible: true,
+        shape: {
+          name: 'polylines',
+          icon: '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash" viewBox="0 0 16 16"><path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/></svg>'
+        },
+        markers: [],
+        featureGroup: null,
+      },
+    }
+  },
+  computed: {
+    formatLatLngs(evt) {
+      console.log(evt)
+      console.log(evt.target.value)
+      return '$'
+    }
+  },
+  methods: {
+    setupLeafletMap: function () {
+      this.map = L.map("mapContainer").setView([51.505, -0.09], 4);
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
+    },
+
+    createFeatureGroup(current) {
+      if (current.featureGroup != null) {
+        this.map.removeLayer(current.featureGroup)
+      }
+      current.featureGroup = L.featureGroup().addTo(this.map)
+
+      current.markers.forEach(m => {
+        m.addTo(current.featureGroup)
+      });
+
+      if (current.markers.length > 1) {
+        if (current.shape.name === 'polylines') {
+          L.polyline(current.latLngs, {color: '#' + current.color}).addTo(current.featureGroup);
+        } else if (current.shape.name === 'rectangle') {
+          L.rectangle(current.featureGroup.getBounds(), {
+            color: '#' + current.color,
+            weight: 1
+          }).addTo(current.featureGroup);
+        } else if (current.shape.name === 'polygon') {
+          if (current.latLngs.length > 2) {
+            L.polygon(current.latLngs, {color: current.color}).addTo(current.featureGroup);
+          } else {
+            this.$toast.add({
+              severity: 'warn',
+              summary: 'Warning',
+              detail: 'A polygon requires more than 2 waypoints',
+              life: 3000
+            });
+          }
+        }
+      }
+
+      this.map.fitBounds(current.featureGroup.getBounds(), {padding: [100, 100]});
+    },
+
+    formatWaypoints(evt) {
+      let inputValue = evt.target.value
+
+      this.currentWaypoints.latLngs = [];
+      this.currentWaypoints.formattedLatLngs = "";
+      this.currentWaypoints.markers = [];
+
+      let coordinates = inputValue.split('\n').filter(itm => itm !== "");
+      coordinates.forEach((c, index) => {
+        if (index > 0) {
+          this.currentWaypoints.formattedLatLngs += ' / '
+        }
+        let strCoordinates = c.split(/[\s,; ]+/);
+        let latLng = [Number(strCoordinates[0]), Number(strCoordinates[1])]
+        this.currentWaypoints.formattedLatLngs += `${strCoordinates[0]}, ${strCoordinates[1]}`
+        this.currentWaypoints.latLngs.push(latLng);
+        this.currentWaypoints.markers.push(L.circleMarker(latLng, {color: '#' + this.currentWaypoints.color}))
+      });
+
+      this.createFeatureGroup(this.currentWaypoints)
+    },
+
+    saveWaypoints() {
+      if (this.currentWaypoints.coordinates === "" || this.currentWaypoints.name === "") {
+        alert("Empty field")
+      } else {
+        this.savedWaypoints.push(this.currentWaypoints);
+        this.waypointInputValue = "";
+        this.currentWaypoints = {
+          formattedLatLngs: "",
+          latLngs: [],
+          color: '1976D2',
+          name: '',
+          visible: true,
+          shape: this.shapes[0],
+          markers: [],
+          featureGroup: null,
+        };
+      }
+    },
+
+    deleteSavedWaypoint(index) {
+      this.map.removeLayer(this.savedWaypoints[index].featureGroup)
+      this.savedWaypoints.splice(index, 1)
+    },
+
+    ddToDms(number) {
+      let d = Math.trunc(number)
+      let m = Math.trunc((number - d) * 60)
+      let s = Math.trunc((number - d - m / 60) * 3600)
+      this.converter.ddToDmsResult = `${d}° ${m}' ${s}"`
+    },
+
+    dmsToDd(values) {
+      this.converter.dmsToDdResult = (values.degrees + values.minutes / 60 + values.seconds / 3600).toFixed(5) + ' °'
+    },
+  },
+  mounted() {
+    this.setupLeafletMap();
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+::v-deep .p-accordion-tab {
+  margin-bottom: 0.5rem;
+}
+
+#menu-bar {
+  height: 5vh;
+
+  #logo {
+    height: 3.5vh;
+    width: auto
+  }
+}
+
+#side-container {
+  z-index: 2;
+  position: fixed;
+  top: calc(5vh + 1em);
+  right: 1em;
+  width: 400px;
+}
+
+#mapContainer {
+  z-index: 1;
+  width: 100vw;
+  height: 95vh;
+}
+
+
+</style>
